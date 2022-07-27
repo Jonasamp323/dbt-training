093b160c057217d25ae8ddc70f4a4cba36461abf
jonathan@ikhokha.com <jonathan@ikhokha.com>
2022-07-27 09:29:48 +0000

    add stage orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,3 +1,24 @@
-select *,
+select
+--from raw_orders
+orderid,
+orderdate,
+shipdate,
+shipmode,
+o.customerid,
+o.productid,
+ordersellingprice,
+ordercostprice,
+--from raw_customer
+customername,
+segment,
+country,
+--from raw_product
+category,
+productname,
+subcategory,
 ordersellingprice - ordercostprice as orderprofit
-from {{ ref('raw_orders') }}
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c
+on o.customerid = c.customerid
+left join {{ ref('raw_products') }} as p
+on o.productid = p.productid
